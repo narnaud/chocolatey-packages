263735be87d832528988a17baa89896a8c929236
Nicolas Arnaud-Cormos <nicolas.arnaudcormos@gmail.com>
2014-02-09 12:09:09 +0100

    Add Dr Memory to user path

--- drmemory.install/tools/chocolateyInstall.ps1
+++ drmemory.install/tools/chocolateyInstall.ps1
@@ -1,4 +1,18 @@
 $packageName = 'drmemory.install'
 $url = 'https://drmemory.googlecode.com/files/DrMemory-Windows-1.6.1-2.exe'
-$installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-Install-ChocolateyPackage "$packageName" 'exe' '/S' "$url"
+
+try {
+  $installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  Install-ChocolateyPackage "$packageName" 'exe' '/S' "$url"
+  
+  $programFiles = if ($isSytem32Bit) { $Env:ProgramFiles } else { ${Env:ProgramFiles(x86)} }
+  $binDir = join-path $programFiles "Dr. Memory/bin"
+  Install-ChocolateyPath "$binDir"
+  
+  Write-ChocolateySuccess "$packageName"
+}
+catch {
+  Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
+  throw
+}
+
