5eb2ed97e94f0b8836cd1b74de5a81537b51486e
Nicolas Arnaud-Cormos <nicolas.arnaudcormos@gmail.com>
2014-02-09 11:52:03 +0100

    Simplify package

--- jpegtran/tools/chocolateyInstall.ps1
+++ jpegtran/tools/chocolateyInstall.ps1
@@ -1,14 +1,4 @@
 $packageName = 'jpegtran'
 $url = 'http://jpegclub.org/jpegtran.zip'
-
-try {
-  $installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-
-  Install-ChocolateyZipPackage "$packageName" "$url" "$installDir"
-
-  Write-ChocolateySuccess "$packageName"
-}
-catch {
-  Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
-  throw
-}
+$installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+Install-ChocolateyZipPackage "$packageName" "$url" "$installDir"
