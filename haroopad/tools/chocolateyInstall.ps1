05c573dcdde978bb44c75946c069402dabc808ba
Nicolas Arnaud-Cormos <nicolas@kdab.com>
2014-05-14 20:24:20 +0200

    Update Haroopad to version 0.12.0

--- haroopad/tools/chocolateyInstall.ps1
+++ haroopad/tools/chocolateyInstall.ps1
@@ -1,4 +1,4 @@
 $packageName = 'haroopad'
-$url = 'https://dl.dropbox.com/s/aqz9deb54le3b1b/haroopad-v0.11.1-i386.msi'
+$url = 'https://dl.dropbox.com/s/7fwdzam2ipfoqg6/haroopad-v0.12.0-i386.msi'
 $installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 Install-ChocolateyPackage "$packageName" 'MSI' '/qb' "$url"
