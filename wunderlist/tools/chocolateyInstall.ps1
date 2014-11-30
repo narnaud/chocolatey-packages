8945ff4d39728ac741c57a50967a39c90913f88e
Nicolas Arnaud-Cormos <nicolas.arnaudcormos@gmail.com>
2014-11-30 20:12:28 +0100

    Update Wunderlist package

--- wunderlist/tools/chocolateyInstall.ps1
+++ wunderlist/tools/chocolateyInstall.ps1
@@ -1,4 +1,4 @@
 $packageName = 'wunderlist'
-$url = 'http://windows.wunderlist.com/download/Wunderlist-Setup.exe'
+$url = 'https://www.wunderlist.com/download/windows7'
 $installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-Install-ChocolateyPackage "$packageName" 'exe' '/S' "$url"
+Install-ChocolateyPackage "$packageName" 'msi' '/quiet' "$url"
