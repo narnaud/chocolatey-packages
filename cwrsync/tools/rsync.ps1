960b409716a3401604a1cac7ebdbdf14f6bd10dc
Nicolas Arnaud-Cormos <nicolas.arnaudcormos@gmail.com>
2015-01-01 14:43:17 +0100

    Fix cwRsync package after review.

--- cwrsync/tools/rsync.ps1
+++ cwrsync/tools/rsync.ps1
@@ -1,3 +1,3 @@
 $currentDir = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
-$env:Path =  "$currentDir\\cwRsync_5.3.0_Free;" + $env:Path
-& "$currentDir\cwRsync_5.3.0_Free\rsync.exe" $args
+$env:Path =  "$currentDir\\cwRsync_5.4.1_x86_Free;" + $env:Path
+& "$currentDir\cwRsync_5.4.1_x86_Free\rsync.exe" $args
