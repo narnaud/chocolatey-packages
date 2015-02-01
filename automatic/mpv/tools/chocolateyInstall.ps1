87eab865dc74a8a0a7b22a02ca6e6cc928c47c8e
Nicolas Arnaud-Cormos <nicolas.arnaudcormos@gmail.com>
2015-02-01 15:03:07 +0100

    Udate mpv to 0.7.3

---
+++ automatic/mpv/tools/chocolateyInstall.ps1
@@ -0,0 +1,5 @@
+﻿$packageName = 'mpv'
+$url = '{{DownloadUrl}}'
+$url64 = '{{DownloadUrlx64}}'
+
+Install-ChocolateyZipPackage "$packageName" "$url" "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" "$url64"
