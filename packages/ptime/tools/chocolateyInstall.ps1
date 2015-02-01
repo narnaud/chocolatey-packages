35c564642eb1625e8cac18f3e4a3e845a5962985
Nicolas Arnaud-Cormos <nicolas.arnaudcormos@gmail.com>
2015-02-01 21:12:06 +0100

    Move ptime to packages

---
+++ packages/ptime/tools/chocolateyInstall.ps1
@@ -0,0 +1,4 @@
+﻿$packageName = 'ptime' # arbitrary name for the package, used in messages
+$url = 'http://www.pc-tools.net/files/win32/freeware/ptime-10.zip'
+
+Install-ChocolateyZipPackage "$packageName" "$url" "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
