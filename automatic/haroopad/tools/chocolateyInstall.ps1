3ec48c9d7c71619ca9ae7c23884ff5f51231f660
Nicolas Arnaud-Cormos <nicolas.arnaudcormos@gmail.com>
2015-02-01 21:53:38 +0100

    Move haroopad to automatic

---
+++ automatic/haroopad/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'haroopad'
+$installerType = 'MSI'
+$url = '{{DownloadUrl}}'
+$url64 = '{{DownloadUrlx64}}'
+$silentArgs = '/qb'
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"
--- haroopad/tools/chocolateyInstall.ps1
+++
@@ -1,5 +0,0 @@
-$packageName = 'haroopad'
-$url = 'https://bitbucket.org/rhiokim/haroopad-download/downloads/Haroopad-v0.13.0-win-ia32.msi'
-$url64 = 'https://bitbucket.org/rhiokim/haroopad-download/downloads/Haroopad-v0.13.0-win-x64.msi'
-$installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-Install-ChocolateyPackage "$packageName" 'MSI' '/qb' "$url" "$url64"
