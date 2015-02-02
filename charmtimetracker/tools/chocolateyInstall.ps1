de9e23703abae0def7822185be275c6c2234ff5f
Nicolas Arnaud-Cormos <nicolas.arnaudcormos@gmail.com>
2015-02-02 21:57:33 +0100

    Move charmtimetracker to packages

--- charmtimetracker/tools/chocolateyInstall.ps1
+++
@@ -1,4 +0,0 @@
-$packageName = 'charmtimetracker'
-$url = 'http://www.kdab.com/~frank/Charm/Charm%201.8.0.exe'
-$installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-Install-ChocolateyPackage "$packageName" 'exe' '/S' "$url"
---
+++ packages/charmtimetracker/tools/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+﻿$packageName = 'charmtimetracker'
+$installerType = 'exe'
+$url = 'http://www.kdab.com/~frank/Charm/Charm%201.8.0.exe'
+$silentArgs = '/S'
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"
