6f70916cfe236b5d9e40f19145db1b67dc482d80
Nicolas Arnaud-Cormos <nicolas.arnaudcormos@gmail.com>
2015-02-02 21:45:25 +0100

    Move qtbinpatcher to packages

--- packages/qtbinpatcher/tools/chocolateyInstall.ps1
+++ packages/qtbinpatcher/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
-$packageName = 'qtbinpatcher'
+﻿$packageName = 'qtbinpatcher'
 $url = 'http://www.tver-soft.org/component/phocadownload/category/13-qtbinpatcher211?download=69:qtbinpatcher-211-win-x86'
 $url64 = 'http://www.tver-soft.org/component/phocadownload/category/13-qtbinpatcher211?download=67:qtbinpatcher-211-win-x64'
-$installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-Install-ChocolateyZipPackage "$packageName" "$url" "$installDir" "$url64"
+
+Install-ChocolateyZipPackage "$packageName" "$url" "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" "$url64"
