dfdf1415d5be3ddde091a2cf152f35f6dbb9de1c
Nicolas Arnaud-Cormos <nicolas.arnaudcormos@gmail.com>
2015-02-03 21:24:23 +0100

    Move wunderlist to packages, and update to version 3.2.1

---
+++ packages/wunderlist/tools/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+﻿$packageName = 'wunderlist'
+$installerType = 'msi'
+$url = 'https://www.wunderlist.com/download/windows7'
+$silentArgs = '/quiet'
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"
--- wunderlist/tools/chocolateyInstall.ps1
+++
@@ -1,4 +0,0 @@
-$packageName = 'wunderlist'
-$url = 'https://www.wunderlist.com/download/windows7'
-$installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-Install-ChocolateyPackage "$packageName" 'msi' '/quiet' "$url"
