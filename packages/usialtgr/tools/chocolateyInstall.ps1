750284ffbd6613b22686169688d465a0e988f0c5
Nicolas Arnaud-Cormos <nicolas.arnaudcormos@gmail.com>
2015-02-03 22:26:54 +0100

    Move usialtgr, and update it to automatically install

--- packages/usialtgr/tools/chocolateyInstall.ps1
+++ packages/usialtgr/tools/chocolateyInstall.ps1
@@ -1,18 +1,23 @@
-$packageName = 'usialtgr'
+﻿$packageName = 'usialtgr'
 $url = 'https://usialtgr.googlecode.com/files/usialtgr_v1.4.zip'
 
 try {
   $tempDir = "$env:TEMP\chocolatey\$($packageName)"
-  if (![System.IO.Directory]::Exists($tempDir)) {[System.IO.Directory]::CreateDirectory($tempDir)}
+  if (![System.IO.Directory]::Exists($tempDir)) {
+    [System.IO.Directory]::CreateDirectory($tempDir)
+  }
   
-  $installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-  
+  $scriptDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  $installerAuto = Join-Path $scriptDir 'usialtgr.au3';  
+
   $zipfile = Join-Path $tempDir "$($packageName).zip"
   Get-ChocolateyWebFile "$packageName" "$zipfile" "$url"  
   Get-ChocolateyUnzip "$zipfile" "$tempDir"
 
+  Write-Host "Installing `'$packageName`' with AutoIt3"
   $exefile = Join-Path $tempDir "setup.exe"
-  Install-ChocolateyInstallPackage "$packageName" 'exe' '' "$exefile"
+  $installArgs = "`"$installerAuto`" `"$exefile`""
+  Start-ChocolateyProcessAsAdmin $installArgs "autoit3"
   
   Write-ChocolateySuccess "$packageName"
 }
@@ -20,4 +25,3 @@ catch {
   Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
   throw
 }
-
---
+++ packages/usialtgr/tools/usialtgr.au3
@@ -0,0 +1,17 @@
+﻿;Path and filename of the installer executable
+$InstallPackage="""" & $CmdLine[1] & """"
+
+; Run installer
+Run($InstallPackage, "")
+If @error <> 0  Then 
+    MsgBox(0, "Run failed", "The ""Run"" command failed with error " & Hex(@error, 8) & " for " & $InstallPackage & " - exiting")
+    Exit
+EndIf
+
+$title="United States-International (AltGr dead keys)"
+
+WinWaitActive($title, "Installation Complete", 10)
+Send("{SPACE}")
+
+;Installation complete
+Exit
