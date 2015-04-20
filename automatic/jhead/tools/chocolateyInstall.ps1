cc69cc3aca8a283d789b3b1a996f935d6ef10fd5
dtgm <dtgm1594@gmail.com>
2015-04-20 16:12:51 -0500

    updated to remove auto-checksum

--- automatic/jhead/tools/chocolateyInstall.ps1
+++ automatic/jhead/tools/chocolateyInstall.ps1
@@ -1,15 +1,11 @@
 $packageName = '{{PackageName}}'
 $url = '{{DownloadUrl}}'
-$checksum = '{{Checksum}}'
-$checksumType = 'sha1'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $installFile = Join-Path $toolsDir "$($packageName).exe"
 try {
   Get-ChocolateyWebFile -PackageName "$packageName" `
                         -FileFullPath "$installFile" `
-                        -Url "$url" `
-                        -Checksum "$checksum" `
-                        -ChecksumType "$checksumType"
+                        -Url "$url"
 } catch {
   throw
 }
