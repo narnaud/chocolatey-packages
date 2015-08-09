3517187de03f40c172af984e98689789c5e1687c
Nicolas Arnaud-Cormos <nicolas.arnaudcormos@gmail.com>
2015-08-09 22:09:36 +0200

    Update todoist package to version 2.7.2

---
+++ packages/todoist/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+$ErrorActionPreference = 'Stop';
+
+$packageName = 'todoist'
+$url = 'https://s3-us-west-1.amazonaws.com/windows.todoist.com/Todoist_for_Windows_2_7_2.exe'
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'EXE'
+  url           = $url
+
+  silentArgs    ='/VERYSILENT'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
