e1615aaf74a318d78891011cba1dbcab2949337c
Nicolas Arnaud-Cormos <nicolas@kdab.com>
2015-12-21 06:57:03 +0100

    Update charmtimetracker

--- packages/charmtimetracker/tools/chocolateyInstall.ps1
+++ packages/charmtimetracker/tools/chocolateyInstall.ps1
@@ -1,15 +1,19 @@
-$ErrorActionPreference = 'Stop';
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
 
-$packageName = 'charmtimetracker'
-$url = 'https://github.com/KDAB/Charm/releases/download/1.10.0/Charm.1.10.0.exe'
+$packageName= 'charmtimetracker' # arbitrary name for the package, used in messages
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://github.com/KDAB/Charm/releases/download/1.10.0/Charm.1.10.0.exe'
 
 $packageArgs = @{
   packageName   = $packageName
+  unzipLocation = $toolsDir
   fileType      = 'exe'
   url           = $url
 
   silentArgs    ='/S'
   validExitCodes= @(0)
+
+  softwareName  = 'Charm'
 }
 
 Install-ChocolateyPackage @packageArgs
---
+++ packages/charmtimetracker/tools/chocolateyuninstall.ps1
@@ -0,0 +1,44 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'charmtimetracker'
+$softwareName = 'Charm'
+$installerType = 'EXE' 
+
+$silentArgs = '/S'
+$validExitCodes = @(0)
+
+$uninstalled = $false
+$local_key     = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*'
+$machine_key   = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*'
+$machine_key6432 = 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*'
+
+$key = Get-ItemProperty -Path @($machine_key6432,$machine_key, $local_key) `
+                        -ErrorAction SilentlyContinue `
+         | ? { $_.DisplayName -like "$softwareName" }
+
+if ($key.Count -eq 1) {
+  $key | % { 
+    $file = "$($_.UninstallString)"
+
+    Uninstall-ChocolateyPackage -PackageName $packageName `
+                                -FileType $installerType `
+                                -SilentArgs "$silentArgs" `
+                                -ValidExitCodes $validExitCodes `
+                                -File "$file"
+  }
+} elseif ($key.Count -eq 0) {
+  Write-Warning "$packageName has already been uninstalled by other means."
+} elseif ($key.Count -gt 1) {
+  Write-Warning "$key.Count matches found!"
+  Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
+  Write-Warning "Please alert package maintainer the following keys were matched:"
+  $key | % {Write-Warning "- $_.DisplayName"}
+}
+
+
+## OTHER HELPERS
+## https://github.com/chocolatey/choco/wiki/HelpersReference
+#Uninstall-ChocolateyZipPackage
+#Uninstall-BinFile # Only needed if you added one in the installer script, choco will remove the ones it added automatically.
+#remove any shortcuts you added
+
