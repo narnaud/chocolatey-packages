c9059097dca5ff4d01cae0fb0dde03d5c02b5d8b
Nicolas Arnaud-Cormos <nicolas@kdab.com>
2015-12-21 21:47:23 +0100

    Move cwrsync outside of automatic package

---
+++ packages/cwrsync/tools/chocolateyinstall.ps1
@@ -0,0 +1,22 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName= 'cwrsync'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$zipDir = 'cwRsync_5.4.1_x86_Free'
+$url        = 'https://www.itefix.net/dl/cwRsync_5.4.1_x86_Free.zip'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  url           = $url
+}
+
+Install-ChocolateyZipPackage @packageArgs
+
+# Ignore all executables
+Set-Content -Path ("$toolsDir\$zipDir\ssh.exe.ignore") -Value $null
+Set-Content -Path ("$toolsDir\$zipDir\ssh-keygen.exe.ignore") -Value $null
+Set-Content -Path ("$toolsDir\$zipDir\rsync.exe.ignore") -Value $null
+
+# Install a script that add path to rsync executables before launching it
+Install-ChocolateyPowershellCommand "$packageName" "$toolsDir\rsync.ps1"
--- packages/cwrsync/tools/rsync.ps1
+++ packages/cwrsync/tools/rsync.ps1
@@ -1,3 +1,7 @@
 $currentDir = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
 $env:Path =  "$currentDir\\cwRsync_5.4.1_x86_Free;" + $env:Path
+if (-not (Test-Path $env:HOME))
+{
+    $env:HOME = $env:USERPROFILE
+}
 & "$currentDir\cwRsync_5.4.1_x86_Free\rsync.exe" $args
