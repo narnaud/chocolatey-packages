834b3f50ede0c7febbc75720b31ce6110853c5f9
Nicolas Arnaud-Cormos <nicolas.arnaudcormos@gmail.com>
2015-12-29 22:10:12 +0100

    Update haroopad

--- automatic/haroopad/tools/chocolateyInstall.ps1
+++ automatic/haroopad/tools/chocolateyInstall.ps1
@@ -1,7 +1,21 @@
-﻿$packageName = 'haroopad'
-$installerType = 'MSI'
-$url = '{{DownloadUrl}}'
-$url64 = '{{DownloadUrlx64}}'
-$silentArgs = '/qb'
+﻿$ErrorActionPreference = 'Stop';
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"
+$packageName= 'haroopad'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = '{{DownloadUrl}}'
+$url64      = '{{DownloadUrlx64}}'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'msi'
+  url           = $url
+  url64bit      = $url64
+
+  silentArgs    = "/qn /norestart /l*v `"$env:TEMP\chocolatey\$($packageName)\$($packageName).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641)
+
+  softwareName  = 'Haroopad*'
+}
+
+Install-ChocolateyPackage @packageArgs
