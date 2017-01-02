abe2a67878e76deea8527b3ccb3464058583c45b
Nicolas Arnaud-Cormos <nicolas.arnaudcormos@gmail.com>
2017-01-02 22:09:40 +0100

    Update jpegtran

--- packages/jpegtran/tools/chocolateyInstall.ps1
+++ packages/jpegtran/tools/chocolateyInstall.ps1
@@ -1,4 +1,16 @@
-$packageName = 'jpegtran'
-$url = 'http://jpegclub.org/jpegtran.zip'
-$installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-Install-ChocolateyZipPackage "$packageName" "$url" "$installDir"
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName= 'jpegtran'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'http://jpegclub.org/jpegtran.zip'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  url           = $url
+
+  checksum      = '65679E6BE6C8E814976E636DB7759D37C1604DBB511D71D846A2F625BDA14F15'
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
