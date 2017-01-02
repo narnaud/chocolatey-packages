fea97aadfe861f6eb41ad004a5ceff2dc930565f
Nicolas Arnaud-Cormos <nicolas.arnaudcormos@gmail.com>
2017-01-02 22:31:14 +0100

    Update kpcli

--- packages/kpcli/tools/chocolateyInstall.ps1
+++ packages/kpcli/tools/chocolateyInstall.ps1
@@ -1,15 +1,11 @@
-$packageName = 'kpcli'
-$url = 'http://sourceforge.net/projects/kpcli/files/kpcli-2.7.exe/download'
+﻿$ErrorActionPreference = 'Stop';
 
-try {
-  $installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-  $installFile = join-path $installDir 'kpcli.exe'
+$packageName= 'kpcli'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installFile = join-path $toolsDir 'kpcli.exe'
+$url        = 'https://sourceforge.net/projects/kpcli/files/kpcli-3.1.exe/download'
 
-  Get-ChocolateyWebFile "$packageName" "$installFile" "$url"
+$checksum      = 'B813C97A1CBF2B515B0ED6C7D027B683D483C9C1CD340A32781486E961817A20'
+$checksumType  = 'sha256'
 
-  Write-ChocolateySuccess "$packageName"
-}
-catch {
-  Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
-  throw
-}
+Get-ChocolateyWebFile $packageName "$installFile" "$url" -checksum $checksum -checksumType $checksumType
