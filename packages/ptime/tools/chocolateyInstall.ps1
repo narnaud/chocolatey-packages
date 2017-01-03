65b7698b79c9d90ff3770a98b0d163ed8c3c5615
Nicolas Arnaud-Cormos <nicolas.arnaudcormos@gmail.com>
2017-01-03 21:23:37 +0100

    Update ptime

--- packages/ptime/tools/chocolateyInstall.ps1
+++ packages/ptime/tools/chocolateyInstall.ps1
@@ -1,4 +1,16 @@
-﻿$packageName = 'ptime' # arbitrary name for the package, used in messages
-$url = 'http://www.pc-tools.net/files/win32/freeware/ptime-10.zip'
+﻿$ErrorActionPreference = 'Stop';
 
-Install-ChocolateyZipPackage "$packageName" "$url" "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$packageName= 'ptime'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'http://www.pc-tools.net/files/win32/freeware/ptime-10.zip'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  url           = $url
+
+  checksum      = 'F64612CE77498F75804A5168517D18F80913A542C8FAA875F871F0D1A79EEAE3'
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
