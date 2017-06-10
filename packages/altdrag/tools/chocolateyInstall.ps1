571605cdd29f2c240ea5b270359f49856270e044
Nicolas Arnaud-Cormos <nicolas.arnaudcormos@gmail.com>
2017-06-10 14:31:39 +0200

    Add checksum to Altdrag

--- packages/altdrag/tools/chocolateyInstall.ps1
+++ packages/altdrag/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 
 $packageName = 'altdrag'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = '{{DownloadUrl}}'
+$url        = 'https://github.com/stefansundin/altdrag/releases/download/v1.1/AltDrag-1.1.exe'
 
 $packageArgs = @{
   packageName   = $packageName
@@ -10,6 +10,9 @@ $packageArgs = @{
   fileType      = 'exe'
   url           = $url
 
+  checksum      = '8B2DB17BC274DD9EC5135FCD316A1E22F97A59E3DCB36B2CF457505D3E2DA703'
+  checksumType  = 'sha256'
+  
   silentArgs   = '/S'
 
   softwareName  = 'AltDrag'
