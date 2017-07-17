8928e1330422e418ab5efe28279f61e29b2278a2
Nicolas Arnaud-Cormos <nicolas.arnaudcormos@gmail.com>
2017-07-17 21:15:03 +0200

    Remove unmaintained packages

--- unmaintained-packages/jhead/tools/chocolateyInstall.ps1
+++
@@ -1,8 +0,0 @@
-﻿$ErrorActionPreference = 'Stop';
-
-$packageName= 'jhead'
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = '{{DownloadUrl}}'
-$installFile = Join-Path $toolsDir "$($packageName).exe"
-
-Get-ChocolateyWebFile $packageName $installFile $url
