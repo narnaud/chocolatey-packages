f00fe0e7f6da2a51cadbd449b0930c5509294f19
Nicolas Arnaud-Cormos <nicolas@kdab.com>
2020-03-25 07:05:16 +0100

    Add MyLifeOrganized

---
+++ packages/mylifeorganized/tools/chocolateyInstall.ps1
@@ -0,0 +1,20 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName= 'mylifeorganized'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://www.mylifeorganized.net/downloads/files/MLO-Setup.exe'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = $url
+
+  softwareName  = 'MyLifeOrganized*'
+
+  checksum      = '3E04E9F99356A6D7A9E3632F7D47677A39EC0CF4EF3F0D9CEEABA47DC98CC84D'
+  checksumType  = 'sha256'
+
+  silentArgs   = '/S'
+}
+Install-ChocolateyPackage @packageArgs
